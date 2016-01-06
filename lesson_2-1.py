9a67f86075ffdd9d399eb3a0f4b5d6135f125344
csgray <csgray@alaska.edu>
2016-01-06 14:21:17 -0900

    Initial commit

---
+++ lesson_2-1.py
@@ -0,0 +1,29 @@
+# Lesson 2.1: Introduction to Serious Programming
+
+# Programming is grounded in arithmetic, so it's important
+# to know how programming languages do simple math.
+# Thankfully, Python follows the same math rules people do.
+# See if you can predict the output of this code.
+
+# https://www.udacity.com/course/viewer#!/c-nd000/l-4180729266/m-48652460
+
+print 3
+print 1 + 1
+
+# "What is Programming?" vocabulary
+# computer: a universal machine that can be programmed to do any computation
+# program: what tells the computer what steps to take
+# precise sequence of steps: series of instructions to accomplish a task
+# computation: 
+# high-level language: input that works through an interpreter and not on the
+#   computer directly
+# input: programs that we write
+# interpreter: executes our programs on the computer
+
+print 52 * 3 + 12 * 9
+print (52 * 3) + (12 * 9)
+print 52 * (3 + 12) * 9
+print 365 * 24 * 60 * 60
+
+#First Programming Quiz
+print 7 * 7 * 24 * 60
