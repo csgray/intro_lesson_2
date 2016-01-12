083f0d252c5a130922aa0d893978ebfae64497e9
csgray <csgray@alaska.edu>
2016-01-12 10:14:17 -0900

    Add lesson 2-5 debugging notes

---
+++ lesson_2-5_debugging.py
@@ -0,0 +1,40 @@
+# Debugging Strategies
+
+# 1. Examine error messages when programs crash
+# The last line of Python Tracebacks will tell you what went wrong. Reading
+# backwards from there will tell you more about where the problem occurred.
+
+# 2. Work from example code
+# If your modified code doesn't work, comment it out and do step-by-step
+# modifications to the example code until it does what you want.
+
+# 3. Make sure examples work
+# Just because you find example code doesn't mean it will work in your system.
+# Check the example code you're using to make sure it behaves the way you
+# expect.
+
+# 4. Check (print) intermediate results
+# When your code doesn't crash, but doesn't behave as expected, add print
+# statements to your program to see where in the code things stop behaving
+# correctly.
+
+# 5. Keep and compare old versions
+# When you have a working version of your code, save it before you add to the
+# code. This will give you something to go back to if you introduce too many
+# new bugs.
+
+# Commenting Guidelines
+
+# 1. Don't comment "obvious code"
+# 'print' does what it says on the box.
+
+# 2. Start functions with a comment
+# Include the expected inputs and outputs with an explanation of what the
+# function does
+
+# 3. Keep comments up-to-date
+# Update the comments when you update the code or things get confusing
+
+# 4. Be concise
+# Only cover the most important details. Too many comments is a sign that your
+# code isn't efficient.
