2ad826990e81704dafef64abdcf93949210b5b5c
csgray <csgray@alaska.edu>
2016-01-13 14:07:35 -0900

    Add Lesson 2-7 Solving Problems with working isLeapYear

---
+++ lesson_2-7_problem_solving.py
@@ -0,0 +1,24 @@
+# Given your birthday and the current date, calculate your age 
+# in days. Compensate for leap days. Assume that the birthday 
+# and current date are correct dates (and no time travel). 
+# Simply put, if you were born 1 Jan 2012 and todays date is 
+# 2 Jan 2012 you are 1 day old.
+
+daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def isLeapYear(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    ##
+    # Your code here.
+    ##
+    return days
+
