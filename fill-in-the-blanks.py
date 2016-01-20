ae2daabb4370c35c0acea3f5fbb47dd691223d57
csgray <csgray@alaska.edu>
2016-01-20 11:16:25 -0900

    Replace wisdom_generator with list. Clean up code and add documentation.

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,49 +1,67 @@
 # IPND Stage 2 Final Project
 
-modes = {'easy': [],
-         'medium': [],
-         'hard': [],
+# This is the start of the data structures
+# The questions are quotes from The Lord of the Rings by J. R. R. Tolkien
+
+# Modes is a dictionary of three empty lists keyed to the difficulty levels
+modes = {"easy": [],
+         "medium": [],
+         "hard": [],
          }
 
-modes['easy'].append({'question': """
+# Appends the first easy question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["easy"].append({"question": """
 It's a dangerous business, Frodo, going out your ___1___. You step onto the
 ___2___, and if you don't keep your ___3___, there's no knowing where you might
 be swept off to.
-""", 'answers': ['door', 'road', 'feet'],
+""", "answers": ["door", "road", "feet"],
     })
 
-modes['easy'].append({'question': """
+# Appends the second easy question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["easy"].append({"question": """
 It's the ___1___ that's never ___2___ as takes ___3___ to finish.
-""", 'answers': ['job', 'started', 'longest'],
+""", "answers": ["job", "started", "longest"],
     })
 
-modes['easy'].append({'question': """
+# Appends the third easy question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["easy"].append({"question": """
 Yet such is oft the course of ___1___ that move the wheels of the world: small
 ___2___ do them because they must, while the ___3___ of the great are elsewhere.
-""", 'answers': ['deeds', 'hands', 'eyes'],
+""", "answers": ["deeds", "hands", "eyes"],
     })
 
-modes['medium'].append({'question': """
+# Appends the first medium question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["medium"].append({"question": """
 War must be, while we defend our lives against a destroyer who would devour 
 all; but I do not love the bright ___1___ for its sharpness, nor the ___2___ 
 for its swiftness, nor the ___3___ for his glory. I love only that which they 
 ___4___.
-""", 'answers': ['sword', 'arrow', 'warrior', 'defend']
+""", "answers": ["sword", "arrow", "warrior", "defend"]
     })
 
-modes['medium'].append({'question': """
+# Appends the second medium question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["medium"].append({"question": """
 I have passed through ___1___ and deep ___2___, since we parted. I have 
 ___3___ much that I thought I knew, and ___4___ again much that I had ___3___.
-""", 'answers': ['fire', 'water', 'forgotten', 'learned']
+""", "answers": ["fire", "water", "forgotten", "learned"]
     })
 
-modes['medium'].append({'question': """
+# Appends the third medium question keyed to "question"
+# and the the answers as a list keyed to "answers"
+modes["medium"].append({"question": """
 And some things that should not have been forgotten were lost. ___1___ became 
 ___2___. ___2___ became ___3___. And for two and a half thousand years, the
 ___4___ passed out of all knowledge.
-""", 'answers': ["History", "legend", "myth", "ring",]
+""", "answers": ["History", "legend", "myth", "ring",]
     })
 
+# Appends the first hard question keyed to "question"
+# and the the answers as a list keyed to "answers"
 modes["hard"].append({"question": """
 The ___1___? His sense of ___2___ was no less than yours, I deem. You wonder 
 what his ___3___ is, where he came from. And if he was really ___4___ at heart.
@@ -52,6 +70,8 @@ rather have stayed there in peace. ___5___ will make corpses of us all.
 """, "answers": ["enemy", "duty", "name", "evil", "War",]
     })
 
+# Appends the second hard question keyed to "question"
+# and the the answers as a list keyed to "answers"
 modes["hard"].append({"question": """
 ___1___ Rings for the Elven-kings under the sky,
 ___2___ for the Dwarf-lords in halls of stone,
@@ -64,6 +84,8 @@ In the Land of Mordor where the ___5___ lie.
 """, "answers": ["Three", "Seven", "Nine", "One", "Shadows",]
     })
 
+# Appends the third hard question keyed to "question"
+# and the the answers as a list keyed to "answers"
 modes["hard"].append({"question": """
 It's like in the great ___1___, Mr. Frodo. The ones that really mattered. Full
 of ___2___ and ___3___ they were. And sometimes you didn't want to know the
@@ -73,7 +95,32 @@ ___5___ing thing... this shadow. Even ___2___ must ___5___.
 """, "answers": ["stories", "darkness", "danger", "end", "pass"],
     })
 
+# Four quotes from the Adeptus Mechanicus in a list
+wisdom = ["""
+A powerful man is not a man who dissects the universe like a puzzle-box,
+examining it piece by piece and measuring each piece with scientific precision.
+A powerful man has only to look upon the universe to change it.
+    - Techno-Magos Gaelos
+        """,
+        """
+Our enemies may rest but rust never sleeps.
+    - Tech-Priest Jung
+        """,
+        """
+You may say, "It is impossible for a man to become like the Machine."
+And I would reply that only the smallest mind strives to comprehend its limits.
+    - Fabricator-General Kane
+        """,
+        """
+A man may die yet still endure if his work enters the greater work, for time is
+carried upon a current of forgotten deeds, and events of great moment are but
+the culmination of a single carefully placed thought.
+    - Techno-Magos Mojaro
+        """
+    ]
 
+
+# This is the start of the actual code
 from random import randint
 
 
@@ -103,16 +150,25 @@ def wrong_answer(tries, question):
         raise SystemExit
 
 
-def blank(question_dict):
+def quiz(question_dict):
+    """Asks the user to fill in the blanks and determines whether or not the
+    answer is correct after stripping out any capitalization. Congratulates the
+    user and quits the program once all the blanks are filled in.
+    """
     blank = 1
     tries = 0
-    question = question_dict['question']
-    answers = question_dict['answers']
+    question = question_dict["question"]
+    answers = question_dict["answers"]
     print question
     while blank <= len(answers):
-        answer = raw_input("What is the answer to blank #" + str(blank) + "?")
-        if answer == answers[blank - 1]:
-            blank, tries, question = correct_answer(blank, tries, answer, question)
+        answer = raw_input("What is the answer to blank #" + str(blank) + "? ")
+        # Converts the user's answer to all lower case
+        answer = answer.lower()
+        # Compares the user's answer to a lower case version of the dictionary answer
+        # blank - 1 because blank is in human terms but the list starts at 0
+        if answer == answers[blank - 1].lower():
+            # Plugs the dictionary answer back in so that it has the correct capitalization
+            blank, tries, question = correct_answer(blank, tries, answers[blank - 1], question)
         else:
             tries = wrong_answer(tries, question)
     print "Congratulations! You filled in all the blanks!"
@@ -124,59 +180,26 @@ def main_screen():
     or the wisdom generator.
     """
     choice = raw_input("Make your choice: ")
-    if choice == "easy":
-        print "Alright! Here is an easy paragraph:"
-        return blank(modes["easy"][randint(0, 2)])
-    if choice == "medium":
-        print "Alright! Here is a medium paragraph:"
-        return blank(modes["medium"][randint(0, 2)])
-    if choice == "hard":
-        print "Alright! Here is a hard paragraph:"
-        return blank(modes["hard"][randint(0, 2)])
+    # 
+    if choice == "easy" or choice == "medium" or choice == "hard":
+        print "Alright! Here is a " + choice + " paragraph:"
+        return quiz(modes[choice][randint(0, 2)])
+    # Prints a random quote from the wisdom collection
     if choice == "wisdom":
-        wisdom_generator()
-    # Terminates the program if the user types 'quit' or 'exit'
-    if choice == "quit" or "exit":
+        print wisdom[randint(0,3)]
+    # Terminates the program if the user types "quit" or "exit"
+    if choice == "quit" or choice == "exit":
         raise SystemExit
     # Invalid input shows the valid inputs and loops back to the prompt
     else:
         print "That is not a valid input. Try 'easy', 'medium', 'hard', or 'wisdom'."
+        print "You may also 'quit' or 'exit'."
         main_screen()
-
-
-def wisdom_generator():
-    """Randomly selects and prints a quote from the Adeptus Mechanicus."""
-    wisdom = randint(0,3)
-    if wisdom == 0:
-        print """
-A powerful man is not a man who dissects the universe like a puzzle-box,
-examining it piece by piece and measuring each piece with scientific precision.
-A powerful man has only to look upon the universe to change it.
-    - Techno-Magos Gaelos
-        """
-    if wisdom == 1:
-        print """
-Our enemies may rest but rust never sleeps.
-    - Tech-Priest Jung
-        """
-    if wisdom == 2:
-        print """
-You may say, "It is impossible for a man to become like the Machine."
-And I would reply that only the smallest mind strives to comprehend its limits.
-    - Fabricator-General Kane
-        """
-    if wisdom == 3:
-        print """
-A man may die yet still endure if his work enters the greater work, for time is
-carried upon a current of forgotten deeds, and events of great moment are but
-the culmination of a single carefully placed thought.
-    - Techno-Magos Mojaro
-        """
 
 
 # This is the main menu that shows up when the program launches
 # Had to escape out the last backslash
-print '''
+print """
  ______ _ _ _    _____        _______ _               ____  _             _    
 |  ____(_) | |  |_   _|      |__   __| |             |  _ \| |           | |   
 | |__   _| | |____| |  _ __ ____| |  | |__   ___ ____| |_) | | __ _ _ __ | | __
@@ -185,5 +208,6 @@ print '''
 |_|    |_|_|_|  |_____|_| |_|   |_|  |_| |_|\___|    |____/|_|\__,_|_| |_|_|\_\\
 
 There are three difficulties: easy, medium, hard
-Or maybe you want some words of _wisdom_?'''
+Or maybe you want some words of _wisdom_?
+"""
 main_screen()
